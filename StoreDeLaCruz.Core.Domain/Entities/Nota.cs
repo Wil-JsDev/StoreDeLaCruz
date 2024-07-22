e7672eb74651ea649f3559eda21f74c88157a947
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-22 09:19:46 -0700

    chore: actualizacion de las entities

---
+++ StoreDeLaCruz.Core.Domain/Entities/Nota.cs
@@ -0,0 +1,24 @@
+﻿using StoreDeLaCruz.Core.Domain.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreDeLaCruz.Core.Domain.Entities
+{
+    public class Nota
+    {
+        public int NotaId { get; set; }
+
+        public int FolderId { get; set; }
+
+        public string Titulo {  get; set; }
+
+        public string Contenido { get; set; }
+
+        public Prioridad PrioridadTarea { get; set; }
+
+        public DateTime FechaDeCreacion { get; set; }
+    }
+}
