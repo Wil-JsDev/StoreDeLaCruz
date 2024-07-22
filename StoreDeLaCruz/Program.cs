05221dc684e5133f37c075042a24c7522d4990cd
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-22 09:03:31 -0700

    confi: Base de datos renombrada y instalacion de libreria

--- StoreDeLaCruz/Program.cs
+++ StoreDeLaCruz/Program.cs
@@ -1,13 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
+using StoreDeLaCruz.Infraestructura.Persistencia;
 
+var builder = WebApplication.CreateBuilder(args);
+var congiguration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddInfraestructura(congiguration);
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
