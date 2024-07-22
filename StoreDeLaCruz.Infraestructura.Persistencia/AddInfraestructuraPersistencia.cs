8fdf7569971ae43cf41ab6041fc474204e13a9aa
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-22 09:10:37 -0700

    feat: extension method agregado

---
+++ StoreDeLaCruz.Infraestructura.Persistencia/AddInfraestructuraPersistencia.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using StoreDeLaCruz.Infraestructura.Persistencia.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreDeLaCruz.Infraestructura.Persistencia
+{
+    public static class AddInfraestructuraPersistencia
+    {
+        public static void AddInfraestructura(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<StoreDeLaCruzContext>(p =>
+            {
+                p.UseSqlServer(configuration.GetConnectionString("StoreConnection"));
+            });
+        }
+
+    }
+}
