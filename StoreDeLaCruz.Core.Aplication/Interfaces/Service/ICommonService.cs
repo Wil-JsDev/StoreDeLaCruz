c431ec57f5881fa1cc26a519a963ad1109cdd0b8
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-26 15:25:21 -0700

    chore: actualizacion de las interfaces

---
+++ StoreDeLaCruz.Core.Aplication/Interfaces/Service/ICommonService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreDeLaCruz.Core.Aplication.Interfaces.Service
+{
+    public interface ICommonService <T, TI, TU>
+    {
+        Task<IEnumerable<T>> GetAll();
+
+        Task<T> GetById(int id);
+
+        Task<T> Add(TI EntityInsert);
+
+        Task<T> Update(int id,TU Entity);
+
+        Task<T> Delete(int id);
+    }
+}
---
+++ StoreDeLaCruz.Core.Aplication/Interfaces/Service/IFolderService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreDeLaCruz.Core.Aplication.Interfaces.Service
+{
+    public interface IFolderService <T, TI, TU> : ICommonService<T, TI, TU>
+    {
+        Task Search();
+    }
+}
