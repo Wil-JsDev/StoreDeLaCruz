910404fc96203595f0adba4c7630dcffe91b626e
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-27 11:23:01 -0700

    chore: mejora de los DTOs

---
+++ StoreDeLaCruz.Core.Aplication/DTOs/Folder/FolderDTos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreDeLaCruz.Core.Aplication.DTOs.Folder
+{
+    public class FolderDTos
+    {
+        public int FolderId { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ StoreDeLaCruz.Core.Aplication/DTOs/Folder/FolderUpdate.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreDeLaCruz.Core.Aplication.DTOs.Folder
+{
+    public class FolderUpdate
+    {
+        public string Name { get; set; }
+    }
+}
