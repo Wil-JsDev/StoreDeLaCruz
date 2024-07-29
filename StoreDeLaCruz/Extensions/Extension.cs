b92527b961fe2d0a2f3b9bb18321f82b7a8d22e0
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-29 10:12:39 -0700

    feat: implemtancion de los extensins method

---
+++ StoreDeLaCruz/Extensions/Extension.cs
@@ -0,0 +1,15 @@
+﻿namespace StoreDeLaCruz.Extensions
+{
+    public static class Extension
+    {
+        public static void UseSwaggerExtension(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "StoreDeLaCruz");
+            });
+            
+        }
+    }
+}
