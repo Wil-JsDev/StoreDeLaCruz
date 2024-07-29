ecc842125bd4af2ccc6baf829dd79e90199222ad
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-29 10:13:38 -0700

    feat: validaciones implementadas

---
+++ StoreDeLaCruz/Validation/ValidationNotaUpdate.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using StoreDeLaCruz.Core.Aplication.DTOs.Nota;
+
+namespace StoreDeLaCruz.Validation
+{
+    public class ValidationNotaUpdate : AbstractValidator<NotaUpdateDTos>
+    {
+        public ValidationNotaUpdate()
+        {
+            RuleFor(x => x.FolderId).NotEmpty().WithMessage("No se puede dejar este campo vacio");
+            RuleFor(x => x.Titulo).NotEmpty().MinimumLength(10).WithMessage("Mas de 10 caracteres");
+            RuleFor(x => x.Contenido).NotEmpty().WithMessage("Es obligatorio asignar un contenido");
+        }
+    }
+}
---
+++ StoreDeLaCruz/Validation/ValidationNoteInsert.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using StoreDeLaCruz.Core.Aplication.DTOs.Nota;
+
+namespace StoreDeLaCruz.Validation
+{
+    public class ValidationNoteInsert : AbstractValidator<NotaInsertDTos>
+    {
+        public ValidationNoteInsert()
+        {
+            RuleFor(x => x.FolderId).NotEmpty().WithMessage("No se puede dejar este campo vacio");
+            RuleFor(x => x.Titulo).NotEmpty().MinimumLength(10).WithMessage("Mas de 10 caracteres");
+            RuleFor(x => x.Contenido).NotEmpty().WithMessage("Es obligatorio asignar un contenido");
+            RuleFor(x => x.PrioridadTarea).NotEmpty().WithMessage("Debemos de saber tu prioridad");
+        }
+    }
+}
