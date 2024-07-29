8a61454648ff7d4e301d44f9797e32f05a237dd8
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-29 10:06:51 -0700

    build: nuevas dependencias

--- StoreDeLaCruz/Program.cs
+++ StoreDeLaCruz/Program.cs
@@ -1,5 +1,11 @@
 using StoreDeLaCruz.Infraestructura.Persistencia;
 using StoreDeLaCruz.Core.Aplication;
+using StoreDeLaCruz.Extensions;
+using StoreDeLaCruz.Core.Aplication.Mapping;
+using FluentValidation;
+using StoreDeLaCruz.Core.Aplication.DTOs.Folder;
+using StoreDeLaCruz.Validation;
+using StoreDeLaCruz.Core.Aplication.DTOs.Nota;
 
 var builder = WebApplication.CreateBuilder(args);
 var configuration = builder.Configuration;
@@ -9,10 +15,17 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddSwagerExtension();
+//builder.Services.AddVersioning();
 
 builder.Services.AddInfraestructura(configuration);
 builder.Services.AddApplication();
-
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+//Validaciones
+builder.Services.AddScoped<IValidator<FolderInsertDTos>, ValidationFolderDTos>();
+builder.Services.AddScoped<IValidator<FolderUpdate>, ValidationFolderUpdate>();
+builder.Services.AddScoped<IValidator<NotaInsertDTos>, ValidationNoteInsert>();
+builder.Services.AddScoped<IValidator<NotaUpdateDTos>, ValidationNotaUpdate>();
 
 var app = builder.Build();
 
@@ -20,6 +33,7 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    //app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
@@ -27,6 +41,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseAuthentication();
+
+//app.UseSwaggerExtension();
 
 app.MapControllers();
 
