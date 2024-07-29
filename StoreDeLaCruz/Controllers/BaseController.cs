b2e1243ec385685a64a5b587303a71b60bda01a8
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2024-07-29 10:11:41 -0700

    feat: implementacion de los controller

---
+++ StoreDeLaCruz/Controllers/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoreDeLaCruz.Controllers
+{
+    [ApiController]
+    [Route("api/v{version: apiVersion}/[controller]")]
+    public abstract class BaseController : ControllerBase
+    {
+    }
+}
